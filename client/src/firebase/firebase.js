665ddf89ba504bc15de03507d390ef5a7c9ed2ab
c4ei <c4ei.net@gmail.com>
2022-07-26 11:31:47 +0900

    ing

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD__iR2Az3NDW7-a4Cpr6YgM7695XSIg94",
-  authDomain: "kryptonite-token.firebaseapp.com",
-  projectId: "kryptonite-token",
-  storageBucket: "kryptonite-token.appspot.com",
-  messagingSenderId: "876140174765",
-  appId: "1:876140174765:web:3d77b7bf5372578dd05524"
+  apiKey: process.env.REACT_APP_FirbaseAPIkey,
+  authDomain: "ico-c4ei-net.firebaseapp.com",
+  projectId: "ico-c4ei-net",
+  storageBucket: "ico-c4ei-net.appspot.com",
+  messagingSenderId: "17022995359",
+  appId: "1:17022995359:web:37c2a2c559fa3a35260baa",
+  measurementId: "G-4GZMNXKR4S"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
